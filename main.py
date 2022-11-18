7cd8410793b63d10a8aa4486303754c6f429a476
Kyle Spinelli <kylespinelli@gmail.com>
2022-11-18 16:46:24 -0500

    Fixed main

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
 
+from get_video_info import get_video_info
+
 
 def create_driver(config=""):
     return webdriver.Chrome()
 
 
-x
-
 if __name__ == "__main__":
-    main()
+    url_list: list = []
+    driver = create_driver()
+    data: list = []
+
+    for url in url_list:
+        data.append(get_video_info(url, driver))
