b0fde1323806a2c4c2bc898d10007680b78cca85
Cat Mai <catmaimx@gmail.com>
2023-04-27 17:05:52 -0400

    add user profile and config to driver

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ from selenium import webdriver
 
 from get_video_info import get_video_info
 import pandas as pd
+import json
 
-def create_driver(config=""):
-    return webdriver.Chrome()
+def create_driver(config_path=""):
+    # config is a json file
+    if config_path:
+        config = {}
+        config_f = open(config_path, 'r')
+        config_json = json.load(config_f)
+        config_f.close()
+        config['username'] = config_json['username']
+        config['password'] = config_json['password']
+        config['user_data'] = config_json['user_data']
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1540,1080")
+    options.add_argument("--no-sandbox")
+    if config['user_data']:
+        options.add_argument("user-data-dir=" + config['user_data'])
+
+    driver = webdriver.Chrome(options = options)
+    return driver
 
 
 if __name__ == "__main__":
@@ -15,7 +32,7 @@ if __name__ == "__main__":
         "https://www.youtube.com/watch?v="+ i for i in df['videoid']
     ]
 
-    driver = create_driver()
+    driver = create_driver("config.json")
     data: list = []
 
     for url in url_list:
