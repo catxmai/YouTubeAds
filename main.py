62f2f462e5cc4d0c6863dbd7d355b56af29650db
Cat Mai <catmaimx@gmail.com>
2023-04-29 17:18:18 -0400

    output to json

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from get_video_info import get_video_info
 from video_controls import *
 from utils import *
+
 import pandas as pd
+import json
 
 
 if __name__ == "__main__":
@@ -19,7 +21,9 @@ if __name__ == "__main__":
     for url in url_list:
         try:
             data.append(get_video_info(url, driver))
-        except Exception as e:
+        except:
             pass
 
     print(data)
+    with open('output.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=True, indent=4)
