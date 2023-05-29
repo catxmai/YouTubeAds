18ecccdfb35c68f7a1a25720ff11914d0bfe46f2
Cat Mai <catmaimx@gmail.com>
2023-05-29 17:19:05 -0400

    complete json parsing

--- sandbox.py
+++ sandbox.py
@@ -9,25 +9,35 @@ from utils import *
 if __name__ == "__main__":
 
 
-	ads = open("ads/ads_20230517_2238_combined.json", "w", encoding="utf-8")
-	ad_attributes = ["preroll_ad_id", "preroll_ad2_id", "side_ad_site", "promoted_video_title"]
-
-	csv_file = open("ads/preroll_ads_20230517_2238_combined.csv", "w", encoding="utf-8")
-	csv_writer = csv.writer(csv_file,
+	id = "20230527_2230"
+	ads = open(f"ads/ads_{id}.json", "w", encoding="utf-8")
+	side_ad_csv_file = open(f"ads/side_ads_{id}.csv", "w", encoding="utf-8")
+	preroll_ad_csv_file = open(f"ads/preroll_ads_{id}.csv", "w", encoding="utf-8")
+	side_ad_csv_writer = csv.writer(side_ad_csv_file,
+                            delimiter = ",",
+                            quotechar = '"',
+                            quoting = csv.QUOTE_MINIMAL,
+                           lineterminator = "\n")
+	preroll_ad_csv_writer = csv.writer(preroll_ad_csv_file,
                             delimiter = ",",
                             quotechar = '"',
                             quoting = csv.QUOTE_MINIMAL,
                            lineterminator = "\n")
 
+
+	ad_attributes = ["preroll_ad_id", "preroll_ad2_id", "side_ad_site", "promoted_video_title"]
+
 	drops = ['video_id', 'video_title', 'video_url', 'channel_name', 'channel_id', 'video_genre', 'video_description',
 		'date_uploaded', 'likes', 'views','comment_count', 'preroll_ad_reasons', 'preroll_ad_id', 'preroll_ad2_id', 'preroll_ad2_reasons',
-		'side_ad_img', 'side_ad_reasons', 'promoted_video_channel', 'promoted_video_reasons', 'promoted_video_info', 'id',]
+		'promoted_video_channel', 'promoted_video_reasons', 'promoted_video_info', ]
 
-	non_preroll = ["side_ad_site", "side_ad_text", "side_ad_info", "promoted_video_title", "promoted_video_url"]
+	non_preroll = ["side_ad_site", "side_ad_text", "side_ad_info", "promoted_video_title", "promoted_video_url", "side_ad_img", 'side_ad_reasons']
+
+	side_ad_attributes = ["side_ad_site", "side_ad_text", "side_ad_img"]
 
 
 	count = 0
-	with open("output/output_20230517_2238_combined.json", 'r', encoding="utf-8") as f:
+	with open(f"output/output_{id}.json", 'r', encoding="utf-8") as f:
 		for _ in range(3):
 			next(f)
 		for line in f:
@@ -36,27 +46,32 @@ if __name__ == "__main__":
 				for att in ad_attributes:
 					if json_data[att]:
 						json.dump(json_data, ads, ensure_ascii=True)
-						ads.write('\n')
-						
-						for cate in drops + non_preroll:
-							json_data.pop(cate, None)
+						ads.write('\n')	
 
-						header = ["preroll_ad_video_url","preroll_ad_site","preroll_ad_advertiser", "preroll_ad_location", "df_index"]
-						header2 = ["preroll_ad2_video_url","preroll_ad2_site","preroll_ad2_info"]
+						side_header = ["side_ad_img","side_ad_site", "side_ad_text", "side_ad_advertiser", "side_ad_location", "id"] 
+						preroll_header = ["preroll_ad_video_url","preroll_ad_site","preroll_ad_advertiser", "preroll_ad_location", "id"]
+
 						if count == 0:
-							csv_writer.writerow(header)
+							side_ad_csv_writer.writerow(side_header)
+							preroll_ad_csv_writer.writerow(preroll_header)
+
+						if json_data["side_ad_site"]:
+							if json_data['side_ad_info']:
+								advertiser, loc = json_data['side_ad_info']
+							data = [json_data["side_ad_img"], json_data["side_ad_site"], json_data["side_ad_text"], advertiser, loc, json_data["id"]]
+							side_ad_csv_writer.writerow(data)
 
 						if json_data["preroll_ad_video_url"]:
 							if json_data['preroll_ad_info']:
 								advertiser, loc = json_data['preroll_ad_info']
-							data = [json_data["preroll_ad_video_url"], json_data["preroll_ad_site"], advertiser, loc, json_data["df_index"]]
-							csv_writer.writerow(data)
+							data = [json_data["preroll_ad_video_url"], json_data["preroll_ad_site"], advertiser, loc, json_data["id"]]
+							preroll_ad_csv_writer.writerow(data)
 
 						if json_data["preroll_ad2_video_url"]:
 							if json_data['preroll_ad2_info']:
 								advertiser, loc = json_data['preroll_ad2_info']
-							data = [json_data["preroll_ad2_video_url"], json_data["preroll_ad2_site"], advertiser, loc, json_data["df_index"]]
-							csv_writer.writerow(data)
+							data = [json_data["preroll_ad2_video_url"], json_data["preroll_ad2_site"], advertiser, loc, json_data["id"]]
+							preroll_ad_csv_writer.writerow(data)
 
 						count +=1
 						break
@@ -64,29 +79,9 @@ if __name__ == "__main__":
 
 	f.close()
 	ads.close()
-	csv_file.close()
-
-	# wonder if sumblime is injecting "open file" character since would get error if running 2 snippets together
-	# fix by generating ads.json, view + save the ads.json file, then run the loads
-	
-	with open("ads/ads_20230517_2238_combined.json",'r', encoding="utf-8") as f:
-		count = 0
-		for line in f:
-			try:
-				data = json.loads(line)
-				count += 1
-			except:
-				# print(line)
-				print(count)
+	side_ad_csv_file.close()
+	preroll_ad_csv_file.close()
 			
-			
-	# df = pd.read_csv("ads/ads_20230517_2238_combined.csv")
-	# df.drop(['video_id', 'video_title', 'video_url', 'channel_name', 'channel_id', 'video_genre', 'video_description',
-	# 	'date_uploaded', 'likes', 'views','comment_count', 'preroll_ad_reasons', 'preroll_ad_id', 'preroll_ad2_id', 'preroll_ad2_reasons',
-	# 	'side_ad_img', 'side_ad_reasons', 'promoted_video_channel', 'promoted_video_reasons', 'promoted_video_info', 'id'],
-	# 	 axis=1, inplace=True)
-	# print(df)
-	# df.to_csv("ads/ads_20230517_2238_combined.csv")
 
 	project_name = "dontcrimeme"
 	bucket_name = "youtube-ads-2023"
